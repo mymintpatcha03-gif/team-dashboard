2663bfff0e35fd940e9c289fbd3c71bb3769e317
mymintpatcha03-gif <mymintpatcha03@gmail.com>
2026-08-09 20:50:53 +0700

    update service worker cache for August 2026

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crm-dashboard-v17';
+const CACHE_NAME = 'crm-dashboard-v18-august-2026';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
